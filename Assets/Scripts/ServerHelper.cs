1e6b5b86349a7771294c87be178bc04fa81592ba
nazar79 <nazar.grinuk@gmail.com>
2018-05-12 23:19:04 +0300

    Test onMessage v 0.1

--- Assets/Scripts/ServerHelper.cs
+++ Assets/Scripts/ServerHelper.cs
@@ -9,40 +9,7 @@ public static class Global // важные данные, доступные во
     public static SocketIOComponent Socket;
     public static string Login;
     public static string Token;
-    public static string LocationType;
-    public static int HP;
-    public static int DMG;
-    public static int Level;
-    public static int XP;
-    public static int XPForLevel;
-    public static JSONObject[] Items;
-    public static int QuantityItems;
-    public static int Money;
-    public static string Battle;
-    public static string BattleLeader;
-    public static string BattleStep;
-    public static GameObject HeroCard;
-
-    public static JSONObject PrepareData(JSONObject data)
-    {
-        data.AddField("login", Login);
-        data.AddField("token", Token);
-        if (Battle != "") data.AddField("battle", Battle);
-        return data;
-    }
-
-    public static void ChangeProperty(string Name, int Value)
-    {
-        switch (Name)
-        {
-            case "HP":
-                HP += Value;
-                break;
-            case "DMG":
-                DMG += Value;
-                break;
-        }
-    }
+    
 }
 public class ServerHelper : MonoBehaviour {
     public GameObject login;
@@ -52,12 +19,12 @@ public class ServerHelper : MonoBehaviour {
 	void Start () {
         GameObject go = GameObject.Find("SocketIO");
         Global.Socket = go.GetComponent<SocketIOComponent>();
-        Global.Socket.On("Message", onMessage);
+        Global.Socket.On("onMessage", onMessage);
 	}
 	
 	public void onMessage(SocketIOEvent e)
     {
-        Debug.Log(e.data);
+        Debug.Log("sadsa");
     }
 
     public void LogOn()
