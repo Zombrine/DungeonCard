f02d30bfc892156d431ec1d89bb8c6e302b5d499
nazar79 <nazar.grinuk@gmail.com>
2018-05-19 11:32:46 +0300

    Test Server-Client
    Test Server-Client

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour {
+
+    public GameObject canvas;
+
+    public void LoginButton()
+    {
+      canvas.SetActive(true);
+      gameObject.SetActive(false);
+    }
+}
