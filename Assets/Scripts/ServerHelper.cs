689cd1b1806695f51b80f7d5585f699a4a419127
nazar79 <nazar.grinuk@gmail.com>
2018-05-29 14:17:03 +0300

    29,05 StructLocation
    StructLocation() не вызывается

--- Assets/Scripts/ServerHelper.cs
+++ Assets/Scripts/ServerHelper.cs
@@ -10,11 +10,13 @@ public static class Global // важные данные, доступные во
    public static SocketIOComponent socket;
    public static string login;
    public static string token;
+   public static string LocationType;
 }
 
 public class ServerHelper : MonoBehaviour
 {
 
+   [SerializeField] GameObject prefTile;
    public GameObject login;
    public GameObject password;
    public GameObject loadingScreen;
@@ -37,6 +39,9 @@ public class ServerHelper : MonoBehaviour
          case "authorization":
             AuthorizationModule(e.data);
             break;
+         case "location":
+            LocationModule(e.data);
+            break;
       }
    }
 
@@ -50,7 +55,20 @@ public class ServerHelper : MonoBehaviour
             onLogOn(data["param"]);
             break;
          case "register":
-            onRegisterOn(data["param"]);
+            onRegister(data["param"]);
+            break;
+         
+      }
+   }
+
+   public void LocationModule(JSONObject data)
+   {
+      Debug.Log("data: " + data);
+      Debug.Log(data["act"].str);
+      switch (data["act"].str)
+      {
+         case "getLocation":
+            onGetLocation(data["param"]);
             break;
       }
    }
@@ -63,6 +81,7 @@ public class ServerHelper : MonoBehaviour
       if (param["error_code"].n == 0)
       {
          Debug.Log("SUCCESS!!!");
+
          loadingScreen.SetActive(true);
          GameObject go = GameObject.Find("CanvasMain");
          go.SetActive(false);
@@ -73,7 +92,7 @@ public class ServerHelper : MonoBehaviour
          Debug.Log("Error in logOn: " + param["error_code"].str);
    }
 
-   public void onRegisterOn(JSONObject param)
+   public void onRegister(JSONObject param)
    {
       Debug.Log(param);
       Debug.Log(param["error_code"]);
@@ -91,5 +110,40 @@ public class ServerHelper : MonoBehaviour
          textError.text = "Такой никнейм уже используется";
       else
          Debug.Log("Error in logOn: " + param["error_code"].str);
+   }
+
+   public void onGetLocation(JSONObject param)
+   {
+      Debug.Log(param);
+      Debug.Log(param["error_code"]);
+      Debug.Log(param.GetField("error_code"));
+      if (param["error_code"].n == 0)
+      {
+         Debug.Log("WORLD!!!");
+         Global.socket.On("StructLocation", StructLocation);
+         //wo.CreateWorld();
+         //GameObject go = GameObject.Find("CanvasMain");
+         //go.SetActive(false);
+      }
+      else
+         Debug.Log("Error in logOn: " + param["error_code"].str);
+   }
+
+   void StructLocation(SocketIOEvent ev)
+   {
+      Debug.Log("StructLocation");
+      for (int j = 0; j < 6; j++)
+      {
+         for (int i = 0; i < 6; i++)
+         {
+            GameObject Temp = Instantiate(prefTile, new Vector3(-49 + (i * 2), 49 - (j * 2), 0f), Quaternion.identity);
+            string SectorName = ev.data["x" + i + "y" + j].str;
+            Temp.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/" + SectorName);
+            Temp.name = SectorName;
+            Temp.transform.parent = (GameObject.Find("CanvasWorld")).transform;
+            Temp.transform.localPosition = new Vector3(-600 * 150 * i, -600 + 150 * j, 0f);
+            Temp.transform.localScale = new Vector3(1, 1, 1);
+         }
+      }
    }
 }
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+using UnityEngine.UI;
+
+public class World : MonoBehaviour
+{
+
+   [SerializeField] GameObject prefTile;
+   // Use this for initialization
+   void Start()
+   {
+      JSONObject data = new JSONObject(JSONObject.Type.OBJECT);
+      data.AddField("module", "location");
+      data.AddField("act", "getLocation");
+      JSONObject param = new JSONObject(JSONObject.Type.OBJECT);
+      data.AddField("param", param);
+
+      /*param.AddField("password", hash);
+      data.AddField("param", param);*/
+      Global.socket.Emit("onMessage", data);
+      
+      Debug.Log("Server");
+   }
+
+   void StructLocation(SocketIOEvent ev) // Построение локации. Вызывается сервером после получение запроса GetLocation
+   {
+      Debug.Log("StructLocation");
+      Global.LocationType = ev.data["Location"].str;
+
+      float Size = ev.data["Size"].n;
+      Size = Mathf.Sqrt(Size);
+      for (int j = 0; j < Size; j++)
+      {
+         for (int i = 0; i < Size; i++)
+         {
+            GameObject Temp = Instantiate(prefTile, new Vector3(0 + 150 * i, 0 + 150 * j, 0f), Quaternion.identity);
+            string SectorName = ev.data["x" + i + "y" + j].str;
+            Temp.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/" + SectorName);
+            Temp.name = SectorName;
+            Temp.transform.parent = (GameObject.Find("CanvasWorld")).transform;
+            Temp.transform.localPosition = new Vector3(0 + 150 * i, 0 + 150 * j, 0f);
+            Temp.transform.localScale = new Vector3(1, 1, 1);
+         }
+      }
+   }
+}
